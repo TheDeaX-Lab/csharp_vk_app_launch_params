8af6bc5f8da5e227b96bbb98efcb7ebc60addae3
TerryChan2003 <aleksandr_h2001@mail.ru>
2019-12-28 12:24:05 +0300

    remove debug

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Web;
 
 namespace ConsoleApp2
@@ -10,38 +11,39 @@ namespace ConsoleApp2
     class Program
     {
 
-        private const String SecretKey1 = "aFZjGLBipm93ruTXZ7Fr";
-        private const String UrlString1 = "https://thedeax-lab.github.io/vk_pikap_master_vkui_edition/?vk_access_token_settings=friends%2Cstories&vk_app_id=7185084&vk_are_notifications_enabled=0&vk_is_app_user=0&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_user_id=258201990&sign=XJitJyxlaBD1QAWbqawaVg2yvTsQ9pz6Y19e8idFhkE";
-        private static readonly Encoding StringEncoder = Encoding.UTF8;
+        private const String SecretKey = "aFZjGLBipm93ruTXZ7Fr";
+        private const String UrlString = "https://thedeax-lab.github.io/vk_pikap_master_vkui_edition/?vk_access_token_settings=friends%2Cstories&vk_app_id=7185084&vk_are_notifications_enabled=0&vk_is_app_user=0&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_user_id=258201990&sign=XJitJyxlaBD1QAWbqawaVg2yvTsQ9pz6Y19e8idFhkE";
+        private static readonly Encoding StringEncoder = Encoding.Default;
 
-        static Boolean IsValid(IDictionary<String, String> QueryParams, String SecretKey)
+        private static Boolean IsValid(IDictionary<string, string> queryParams, string secretKey)
         {
-            var checkString = QueryParams.Select(v => v)
+            var checkString = queryParams.Select(v => v)
                 .Where(v => v.Key.StartsWith("vk_"))
                 .OrderBy(kvp => kvp.Key)
                 .Select(kvp => $"{kvp.Key}={kvp.Value}")
-                .Aggregate((k1, k2) => $"{k1}&{k2}");
-            checkString = checkString.Replace(",", "%2C");
-            using (var hmac = new HMACSHA256(StringEncoder.GetBytes(SecretKey)))
+                .Aggregate((k1, k2) => $"{k1}&{k2}")
+                .Replace(",", "%2C");
+            using (var hmac = new HMACSHA256(StringEncoder.GetBytes(secretKey)))
             {
-                var sign_byte = hmac.ComputeHash(StringEncoder.GetBytes(checkString));
-                String sign = Convert.ToBase64String(sign_byte);
-                sign = sign.Substring(0, sign.Length - 1);
-                Console.WriteLine("{0} {1}", sign, QueryParams["sign"]);
-                return sign.Equals(QueryParams["sign"]);
+                var signByte = hmac.ComputeHash(StringEncoder.GetBytes(checkString));
+                var sign = Convert.ToBase64String(signByte)
+                    .Replace('+','-')
+                    .Replace("=", string.Empty)
+                    .Replace('/', '_');
+                return sign.Equals(queryParams["sign"]);
             }
         }
 
         static void Main()
         {
-            var Dict = new Dictionary<String, String>();
-            var DecodedParams = HttpUtility.ParseQueryString(UrlString1.Split("?")[1]);
-            foreach (var key in  DecodedParams.AllKeys)
+            var dict = new Dictionary<string, string>();
+            var decodedParams = HttpUtility.ParseQueryString(UrlString.Split("?")[1]);
+            foreach (var key in  decodedParams.AllKeys)
             {
                 Console.WriteLine(key);
-                Dict.Add(key, DecodedParams[key]);
+                dict.Add(key, decodedParams[key]);
             }
-            Console.WriteLine(IsValid(Dict, SecretKey1));
+            Console.WriteLine(IsValid(dict, SecretKey));
         }
     }
 }
